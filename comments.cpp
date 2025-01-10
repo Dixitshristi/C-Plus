4cd77de577f42ab8a8bd172deb94223e092f8837
Shristi-Dixit <140090682+Dixitshristi@users.noreply.github.com>
2025-01-10 21:52:37 +0530

    comments.cpp
    Comments can be used to explain C++ code, and to make it more readable. It can also be used to prevent execution when testing alternative code. Comments can be singled-lined or multi-lined.
    #Single-line Comments
    1.Single-line comments start with two forward slashes (//).
    2.Any text between // and the end of the line is ignored by the compiler.
    
    #Multi-line Comments
    1.Multi-line comments start with /* and ends with */.
    2.Any text between /* and */ will be ignored by the compiler.

---
+++ comments.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //print"Hey_world!"
+    cout<<"Hey_World!";
+
+    cout<<endl;
+   
+    /*
+    print"Hey_world!"
+    print"I'm Shristi Dixit"
+    */
+    cout<<"Hey_World!"<<endl<<"I'm Shristi Dixit";
+     
+    return 0;
+
+}
