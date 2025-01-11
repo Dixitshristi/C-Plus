c81e3b458235e97bd9cc6fde439e12d9c834a728
Shristi-Dixit <140090682+Dixitshristi@users.noreply.github.com>
2025-01-11 21:31:52 +0530

    double.cpp

---
+++ double.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    double x,y;
+    x = 0.5;
+    y = 19.5;
+    
+    cout<<x+y<<endl;
+    cout<<x-y<<endl;
+    cout<<x*y<<endl;
+    cout<<x/y<<endl;
+
+    return 0;
+}
