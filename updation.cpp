753855b9a1f027451126597476f19c3590739122
Shristi-Dixit <140090682+Dixitshristi@users.noreply.github.com>
2025-01-11 13:34:49 +0530

    updation.cpp
    "UPDATION" is also known as "OVERWRITE".

---
+++ updation.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int x=5;
+    cout<<x<<endl;
+    
+    x=10;
+    cout<<x<<endl;
+
+    x=x+25;
+    cout<<x<<endl;
+
+    x=x-15;
+    cout<<x<<endl;
+
+    x=x*30;
+    cout<<x<<endl;
+    
+    return 0;
+}
