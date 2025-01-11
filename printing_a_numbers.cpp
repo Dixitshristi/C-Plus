1b9972db2cf8e31429126ec7abcb8c4f592a951e
Shristi-Dixit <140090682+Dixitshristi@users.noreply.github.com>
2025-01-11 12:51:55 +0530

    printing_a_numbers.cpp

---
+++ printing_a_numbers.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    cout<<4;
+    cout<<endl;
+
+    cout<<"4";
+    cout<<endl;
+
+    cout<<3+4;
+    cout<<endl;
+   
+    cout<<"3+4";
+    cout<<endl;
+
+    return 0;
+}
