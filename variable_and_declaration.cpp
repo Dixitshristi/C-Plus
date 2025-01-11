9d730eeb02e2e6bfbcc87236372cc7cc081df8f9
Shristi-Dixit <140090682+Dixitshristi@users.noreply.github.com>
2025-01-11 21:21:41 +0530

    variable_and_declaration.cpp

---
+++ variable_and_declaration.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int a,b,c; //declaration.
+    a = 10;//initilzation.
+    b = 10;//initilzation.
+    
+
+    cout<<a+b+c;
+    cout<<endl;
+
+    return 0;
+}
