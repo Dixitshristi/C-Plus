74594aac77eeb855a68fe5c7a69cdfa04a44b08b
Shristi-Dixit <140090682+Dixitshristi@users.noreply.github.com>
2025-01-14 15:09:55 +0530

    incre_dre.cpp

---
+++ incre_dre.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int ab;
+    ab=5;
+
+    ab++;//post increment
+    cout<<ab<<endl;
+
+    ab--;//post decrement
+    cout<<ab<<endl;
+
+    ++ab;//pre increment
+    cout<<ab<<endl;
+
+    --ab;//pre decrement
+    cout<<ab<<endl;
+
+    return 0;
+}
