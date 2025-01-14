895c1daa1c5576b5febe29b7da3ccebb4734ea3f
Shristi-Dixit <140090682+Dixitshristi@users.noreply.github.com>
2025-01-14 13:55:57 +0530

    int_arthmetic_operators.cpp

---
+++ int_arthmetic_operators.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+    x = 10;
+    y = 2;
+
+    cout<<x+y<<endl;
+    cout<<x-y<<endl;
+    cout<<x*y<<endl;
+    cout<<x/y<<endl;
+
+    return 0;
+    
+}
