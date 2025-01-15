ba3e96ea4fbacd587eb5f52fa90510c20b557813
Shristi-Dixit <140090682+Dixitshristi@users.noreply.github.com>
2025-01-15 17:44:52 +0530

    simple_interest.cpp

---
+++ exercise_2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float p,r,t;
+
+    //principal
+    p = 50;
+    //rate
+    r = 10;
+    //time
+    t = 200;
+
+    //formula for si
+    float Simple_Interest =(p*r*t)/100;
+
+    //print to display
+    cout<<Simple_Interest<<endl;
+    
+    return 0;
+}
