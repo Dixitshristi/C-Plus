54933c45b9359c09d0c447c243c3c160088ae7d3
Shristi-Dixit <140090682+Dixitshristi@users.noreply.github.com>
2025-01-15 18:48:00 +0530

    variblenamerule.cpp

---
+++ variblenamerule.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int r = 4;
+    cout<<r<<endl;
+
+    int y5 = 76;
+    cout<<y5<<endl;
+
+    int auto =56;
+    cout<<auto<<endl;
+
+    int _y7 = 67;
+    cout<<_y7<<endl;
+
+    return 0;
+
+}
