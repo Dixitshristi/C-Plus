deea7e0ef105a48cd48b7dfe8e7a921496f8791c
Shristi-Dixit <140090682+Dixitshristi@users.noreply.github.com>
2025-01-15 18:18:28 +0530

    vos.cpp

---
+++ vos.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float pie, r;
+    pie = 3.14;
+    r = 10;
+
+    float vos = 1.33*pie*r*r*r;
+
+    cout<<vos<<endl;
+
+    return 0;
+}
