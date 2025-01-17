c1781ed84e7b92e6fd59b18d1234b006a897be2a
Shristi-Dixit <140090682+Dixitshristi@users.noreply.github.com>
2025-01-17 13:24:53 +0530

    exercise_3.cpp

---
+++ exercise_3.cpp
@@ -0,0 +1,17 @@
+//sum of two given numbers
+//by using user input
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+    cout<<"enter your first number : ";
+    cin>>x;
+
+    cout<<"enter your second number : ";
+    cin>>y;
+
+    cout<<"sum of two given numbers : "<< x+y;
+
+    return 0;
+}
