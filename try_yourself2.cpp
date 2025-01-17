67dcab97c1ef432456e6ab8bfe10c3dafc347ae7
Shristi-Dixit <140090682+Dixitshristi@users.noreply.github.com>
2025-01-17 13:36:46 +0530

    try_yourself2.cpp

---
+++ try_yourself2.cpp
@@ -0,0 +1,18 @@
+//sum of three given numbers in c++ y using user input
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int a,b,c;
+    cout<<"Enter your first number : ";
+    cin>>a;
+    cout<<"Enter your second number : ";
+    cin>>b;
+
+    cout<<"Enter your third number : ";
+    cin>>c;
+
+    cout<<"sum of the given three number : " <<a+b+c;
+    
+    return 0;
+}
