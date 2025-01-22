d9a7a6df94c50f660a6b783e6f0632321a187431
Shristi-Dixit <140090682+Dixitshristi@users.noreply.github.com>
2025-01-22 12:57:22 +0530

    typecasting.cpp

---
+++ typecasting.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cout<<"enter your number";
+    cin>>x;
+    
+
+    float y =(float)x; //typecasting method
+    cout<<y/2;
+
+    return 0;
+}
