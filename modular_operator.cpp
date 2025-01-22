d8b6e951a6fb8562c688613022079c177d90164b
Shristi-Dixit <140090682+Dixitshristi@users.noreply.github.com>
2025-01-22 12:29:05 +0530

    Create modular_operator.cpp

---
+++ modular_operator.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int a=5 ,b=9015;
+    //a%b
+    cout<<a%b<<endl;
+    //a%a
+    cout<<a%a<<endl;
+    //a%(-b)
+    cout<<a%(-b)<<endl;
+    //-a%b
+    cout<<-a%b<<endl;
+
+    return 0;
+}
