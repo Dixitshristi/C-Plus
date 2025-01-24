7166267cdb2f9fd559c86f804715969d726ebef0
Shristi-Dixit <140090682+Dixitshristi@users.noreply.github.com>
2025-01-24 13:40:54 +0530

    if_statement.cpp

---
+++ if_statement.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a = 24; 
+    int b = 39;
+    
+    if (a%2==0);
+    {
+        cout<<"EVEN";
+        
+    }
+
+    cout<<endl;
+    
+    if (b!=0)
+    {
+        cout<<"ODD";
+    }
+    
+    return 0;
+}
