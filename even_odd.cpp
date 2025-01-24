0a138c09b6b11f56cf916d1ffe716315f39780e4
Shristi-Dixit <140090682+Dixitshristi@users.noreply.github.com>
2025-01-24 13:16:05 +0530

    even_odd.cpp

---
+++ even_odd.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cout<<"ENTER YOUR VALUE : ";
+    cin>>x;
+
+    if (x%2==0)
+    {
+        cout<<"EVEN";
+    }
+    else
+    {
+        cout<<"ODD";
+    }
+    return 0;
+}
