1a8673006738d85c1a61246b8271330fa14dada4
Shristi-Dixit <140090682+Dixitshristi@users.noreply.github.com>
2025-01-26 21:59:22 +0530

    ab.cpp

---
+++ ab.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"ENTER YOUR VALUE : ";
+    cin>>n;
+
+    if (n>=0)
+    {
+        cout<<n;
+    }
+    else
+    {
+        cout<<-n;
+    }
+    return 0;
+}
