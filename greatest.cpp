f950b80f78eb20b43916e8cafaab95a2925843ab
Shristi-Dixit <140090682+Dixitshristi@users.noreply.github.com>
2025-01-30 05:23:24 +0530

    greatest.cpp

---
+++ greatest.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"ENTER YOUR a VALUE : ";
+    cin>>a;
+    int b;
+    cout<<"ENTER YOUR b VALUE : ";
+    cin>>b;
+    int c;
+    cout<<"ENTER YOUR c VALUE : ";
+    cin>>c;
+
+    if (a>b && a>c)
+    {
+        cout<<c;
+    }
+    else if (b>a && b>c)
+    {
+        cout<<b;
+    }
+    else
+    {
+        cout<<c;
+    }
+    return 0;
+}
