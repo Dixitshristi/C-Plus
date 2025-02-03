cb90c8fe8c34d1de7ba6c6374c5c860cc12b2128
Shristi-Dixit <140090682+Dixitshristi@users.noreply.github.com>
2025-02-03 15:38:36 +0530

    for.cpp

---
+++ for.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"ENTER YOUR VALUE : ";
+    cin>>n;
+    
+    for (int i = 1; i <=n; i++)
+    {
+        cout<<"hello_world!"<<endl;
+        //break;
+    }
+
+    return 0;
+}
