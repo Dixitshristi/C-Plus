a4b8d118402d45e78d045ef9458d804271ab28d2
Shristi-Dixit <140090682+Dixitshristi@users.noreply.github.com>
2025-02-06 19:14:48 +0530

    break.cpp

---
+++ break.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter your value : ";
+    cin>>n;
+    int f=1;
+
+
+    for (int i = 1; i <n; i++)
+    {
+        if (n%i==0)  f=i;
+       
+    }
+    cout<<f;
+    return 0;
+}
