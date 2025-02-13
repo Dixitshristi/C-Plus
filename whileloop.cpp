dfe694332e0f5425f2a7f64ddd36f6993ea237b1
Shristi-Dixit <140090682+Dixitshristi@users.noreply.github.com>
2025-02-13 15:26:04 +0530

    whileloop.cpp

---
+++ whileloop.cpp
@@ -0,0 +1,13 @@
+//wap to print 1 to 100 numbers using while loop in c++.
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i=1;
+    while(i<100)
+    {
+        cout<<i<<" ";
+        i++;
+    }
+    return 0;
+}
