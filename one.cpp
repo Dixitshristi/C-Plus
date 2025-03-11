bca5c74e93c49560ef373a2e76321ea5f2443854
Shristi-Dixit <140090682+Dixitshristi@users.noreply.github.com>
2025-03-11 16:17:38 +0530

    one.cpp

---
+++ one.cpp
@@ -0,0 +1,16 @@
+//print the given pattern
+#include<iostream>
+using namespace std;
+int main()
+{
+    //recatangle
+    //r=m, c=n=5
+    int m;
+    cout<<"ENTER YOUR ROW VALUE:";
+    cin>>m;
+    for (int i = 1; i<=m; i++)
+    {
+        cout<<"*****"<<endl;
+    }
+    return 0;    
+}
